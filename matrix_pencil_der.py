7dff59c9fb026359c9e6a38d404ea72232cdac29
Jan Filipe Kiviaho <jfkiviaho@gatech.edu>
2018-04-13 01:25:42 -0400

    there's a problem with differentiating Re()

--- matrix_pencil_der.py
+++ matrix_pencil_der.py
@@ -23,6 +23,30 @@ def DcDalpha(alphas, rho):
 
     return np.exp(rho*(alphas - m))/a
 
+def DalphaDlam(lam, dt):
+    """
+    Derivative of exponents with respect to eigenvalues
+
+    Parameters
+    ----------
+    lam : numpy.ndarray
+        eigenvalues
+    dt : float
+        time step
+
+    Returns
+    -------
+    numpy.ndarray
+        derivatives
+
+    """
+    return np.real((1.0/lam))/dt
+
+def EigenDerivative():
+    """
+    Derivatives of each eigenvalue with respect to originating matrix
+    """
+
 def SVDDerivative():
     """
     Derivatives of SVD of rectangular matrix of size m x n
--- matrix_pencil_test.py
+++ matrix_pencil_test.py
@@ -29,9 +29,46 @@ def TestDcDalpha():
     rel_error = (analytic - approx)/approx
     print "Rel. error:    ", rel_error
 
+    return
 
+def TestDalphaDlam():
+    """
+    Test DcDalpha function using random data and finite difference
+    approximation
+    """
+    # Create random complex eigenvalues
+    M = 10
+    dt = 0.1
+    alphas = -1.0 + 2.0*np.random.random(M) 
+    omegas = -1.0 + 2.0*np.random.random(M) 
+    s = alphas + 1j*omegas
+    lam = np.exp(s*dt)
+
+    # Perturb the eigenvalues 
+    h = 1.0e-6
+    pert = np.ones(M) + 1j*np.ones(M)
+    lam_pos = lam + h*pert
+    lam_neg = lam - h*pert
+
+    # Compute the perturbed alphas
+    alpha_pos = np.log(lam_pos).real/dt
+    alpha_neg = np.log(lam_neg).real/dt
+
+    # Compute finite difference approximatation to derivatives
+    approx = 0.5*(alpha_pos - alpha_neg)/h
+    print "Approximation: ", approx
+
+    # Obtain derivatives analytically
+    analytic = DalphaDlam(lam, dt)
+    print "Analytic:      ", analytic
+
+    # Compute relative error
+    rel_error = (analytic - approx)/approx
+    print "Rel. error:    ", rel_error
 
 if __name__ == "__main__":
     print "Testing derivative of KS function..."
     TestDcDalpha()
+    print "Testing derivative of exponent extraction..."
+    TestDalphaDlam()
 
