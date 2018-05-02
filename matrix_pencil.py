fixed damping sign convention

matrix_pencil.py
@@ -47,7 +47,6 @@ class MatrixPencil(object):
             self.X = self.H.dot(x)
             self.N = self.X.shape[0]
             self.dt = T[1] - T[0]
-            print la.norm(self.X)
 
         # Set the pencil parameter L
         self.n = self.X.shape[0]
@@ -141,7 +140,7 @@ class MatrixPencil(object):
 
         # Compute damping and freqency
         s = np.log(self.lam[:self.M])/self.dt
-        self.damp = s.real
+        self.damp = -s.real
         self.freq = s.imag
 
         return
@@ -196,9 +195,9 @@ class MatrixPencil(object):
             approximate maximum of real part of exponents
 
         """
-        m = self.damp.max()
+        m = -self.damp.min()
         
-        return m + np.log(np.sum(np.exp(self.rho*(self.damp - m))))/self.rho
+        return -(m + np.log(np.sum(np.exp(self.rho*(-self.damp - m))))/self.rho)
 
     def AggregateDampingDer(self):
         """
@@ -260,6 +259,6 @@ class MatrixPencil(object):
             w = self.freq[i]
             p = self.faze[i]
 
-            X += a*np.exp(x*t)*np.cos(w*t + p)
+            X += a*np.exp(-x*t)*np.cos(w*t + p)
 
         return X

matrix_pencil_der.py
@@ -12,9 +12,9 @@ def c_ks(alphas, rho):
         approximate maximum of real part of exponents
 
     """
-    m = alphas.max()
+    m = alphas.min()
     
-    return m + np.log(np.sum(np.exp(rho*(alphas - m))))/rho
+    return -m + np.log(np.sum(np.exp(-rho*(alphas - m))))/rho
 
 def DcDalpha(alphas, rho):
     """
@@ -33,10 +33,10 @@ def DcDalpha(alphas, rho):
         array of derivatives
 
     """
-    m = alphas.max()
-    a = np.sum(np.exp(rho*(alphas - m)))
+    m = -alphas.min()
+    a = np.sum(np.exp(rho*(-alphas - m)))
 
-    return np.exp(rho*(alphas - m))/a
+    return -np.exp(rho*(-alphas - m))/a
 
 def DalphaDlam(lam, dt):
     """

spring_mass_damper.py
@@ -12,7 +12,7 @@ k = 7000.0  # N/m
 
 wn = np.sqrt(k/m)
 print "exact frequency: ", np.sqrt(k/m)
-print "exact damping:   ", -0.5*c/m
+print "exact damping:   ", 0.5*c/m
 
 A = np.array([[0, 1], [-k / m, -c / m]])
 